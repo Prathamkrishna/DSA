b20f1aee1bcf7b325f4943ef0039cfc73fe77d3b
Pratham Krishna <pratham.krishna.06@gmail.com>
2021-05-19 14:30:21 +0530

    added popElement to stack.cpp

--- DataStructures/C++/stack.cpp
+++ DataStructures/C++/stack.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 int pushingElement(int x);
+int popElement();
 
 vector<int>arr1(5);
 int i = 0;
+int x = 4;
 
 int main(){
     pushingElement(5);
@@ -14,16 +16,30 @@ int main(){
     for(int j = 0; j < 5; j++){
         cout<<arr1[j]<<endl;
     }
+    popElement();
+    popElement();
     return 0;
 }
 
 int pushingElement(int x){
     if(i > 4){
-        cout<<"cannot be added"<<endl;
+        cout<<"stack overflow"<<endl;
     }
     else{
         arr1[i] = x;
         i++;
+    }
+    return 0;
+}
+
+int popElement(){
+    if(x < 0){
+        cout<<"stack underflow"<<endl;
+    }
+    else{
+        vector<int>arr1(x);
+        cout<<"the number of elements in this array are: "<<x<<endl;
+        x--;
     }
     return 0;
 }
