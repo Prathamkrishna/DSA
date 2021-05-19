49422549ce0291ff29bec5cd859c24ca4065cf09
Pratham Krishna <pratham.krishna.06@gmail.com>
2021-05-19 20:30:17 +0530

    changed individual functions to a class in stack.cpp

--- DataStructures/C++/stack.cpp
+++ DataStructures/C++/stack.cpp
@@ -1,48 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int pushingElement(int x);
-int popElement();
+class Stacks{
+    public:
+    int arr1[20];
+    // vector<int>arr1
+    int topmost = 0;
 
-vector<int>arr1(5);
-int i = 0;
-int x = 4;
+    int pushingElement(int y){  
+        arr1[topmost] = y;
+        cout<<topmost<<"value"<<y<<endl;
+        topmost++;
+        return 0;
+    }
+
+    int popElement(){
+        arr1[--topmost];
+        cout<<"the number of elements in this array are: "<<topmost<<endl;
+        for(int i = 0; i < topmost; i++){
+            cout<<arr1[i]<<endl;
+        }
+        return 0;
+    }
+};
 
 int main(){
-    pushingElement(5);
-    pushingElement(4);
-    pushingElement(3);
-    pushingElement(2);
-    for(int j = 0; j < 5; j++){
-        cout<<arr1[j]<<endl;
-    }
-    popElement();
-    popElement();
-    return 0;
-}
-
-int pushingElement(int x){
-    if(i > 4){
-        cout<<"stack overflow"<<endl;
-    }
-    else{
-        arr1[i] = x;
-        i++;
-    }
-    return 0;
-}
-
-int popElement(){
-    if(x < 0){
-        cout<<"stack underflow"<<endl;
-    }
-    else{
-        vector<int>arr1(x);
-        cout<<"the number of elements in this array are: "<<x<<endl;
-        x--;
-    }
-    for(int j = 0; j < x; j++){
-        cout<<arr1[j];
-    }
+    Stacks s;
+    s.pushingElement(5);
+    s.pushingElement(4);
+    s.pushingElement(3);
+    s.pushingElement(2);
+    // for(int j = 0; j < 5; j++){
+    //     cout<<arr1[j]<<endl;
+    // }
+    s.popElement();
+    s.popElement();
     return 0;
 }
